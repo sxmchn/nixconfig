85918e30ab9c2c195a5eb76179fa3416d12fb404
Artem Semenov <sxmchn.org@gmail.com>
2026-05-20 12:53:48 +0000

    add power management

--- configurations/nixos/home-server/configuration.nix
+++ configurations/nixos/home-server/configuration.nix
@@ -14,4 +14,32 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
 	system.stateVersion = "25.11";
+
+  # Intel CPU microcode
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Базовое управление питанием
+  powerManagement.enable = true;
+  powerManagement.cpuFreqGovernor = "powersave";
+
+  # Автонастройка энергосбережения
+  powerManagement.powertop.enable = true;
+
+  # Температурный контроль Intel
+  services.thermald.enable = true;
+
+  # Нормальный sleep/suspend
+  services.logind.settings.Login = {
+    HandlePowerKey = "poweroff";
+    HandleLidSwitch = "ignore";
+  };
+
+  # Для сервера обычно лучше не засыпать автоматически
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
+
 }
